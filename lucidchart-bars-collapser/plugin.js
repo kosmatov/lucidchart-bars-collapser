04148373358457969dac112e9a6ac9d0bb80efc8
Konstantin Kosmatov <key@kosmatov.su>
2012-06-25 13:23:16 +0400

    Initial commit

---
+++ lucidchart-bars-collapser/plugin.js
@@ -0,0 +1,51 @@
+(function ($){
+
+    $(function() {
+
+        var act = function () {
+
+            if ( ! $('div.gui-container > div.option-bar').length ) {
+
+                setTimeout(act, 1000);
+                return;
+            }
+
+            var collapse = function () {
+
+                $('.menu-bar, .tab-bar, .gui-footer').hide();
+                $('.gui-bottom-container').css({top: 25, bottom:0});
+            }
+
+            var expand = function () {
+
+                $('.gui-bottom-container').css({top: 85, bottom:25});
+                $('.menu-bar, .tab-bar, .gui-footer').show();
+            }
+
+            $('div.gui-container > div.option-bar').append(
+                $('<div class="option-bar-input"></div>').append(
+                    $('<div class="button-bar"></div>').append(
+                        $('<div class="button-bar-item"></div>').html('&equiv;').click(function(){
+                            if ($(this).hasClass('cllpsd')){
+
+                                expand();
+                                $(this).removeClass('cllpsd');
+                            }
+
+                            else {
+
+                                collapse();
+                                $(this).addClass('cllpsd');
+                            }
+
+                        }).click()
+                    )
+                ).css({float: 'right', display: 'block', textAlign: 'center', cursor: 'pointer'})
+            );
+
+            setTimeout(function() {$('div.ubergrowl-container').remove()}, 1000);
+        }
+
+        setTimeout(act, 1000);
+    })
+})(jQuery);
